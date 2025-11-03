e8bd3db90e64ff4e36c3931cf906cde294a7dbc8
Shreyas D <shreyas24d@gmail.com>
2025-11-04 02:57:39 +0530

    switching to single server

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ import httpServer from "./socket.js";
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on ${PORT}`);
+// });
 
 httpServer.listen(PORT, () => {
   console.log("Socket server is running on port 3001");
