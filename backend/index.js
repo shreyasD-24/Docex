be01be338aa625083cdacd4842cf3eb8583b6458
Shreyas D <shreyas24d@gmail.com>
2025-11-04 03:10:24 +0530

    Added vercel.json file for frontend and solved port issue in backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,7 @@
-import app from "./app.js";
 import httpServer from "./socket.js";
 
 const PORT = 3000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on ${PORT}`);
-// });
-
 httpServer.listen(PORT, () => {
-  console.log("Socket server is running on port 3001");
+  console.log("HTTP + Socket server is running on port " + PORT);
 });
