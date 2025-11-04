010c02ea4922f973680d06329121853ae67cf0ff
Shreyas D <shreyas24d@gmail.com>
2025-11-05 01:22:23 +0530

    Deployment commit

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -9,7 +9,6 @@ function UserList({ userList, socket, roomId }) {
   let [audioPermissionsGranted, setAudioPermissionsGranted] = useState(false);
 
   const ICE_SERVERS = [
-    { urls: "stun:stun.l.google.com:19302" }, // STUN
     {
       username: import.meta.env.VITE_TURN_USERNAME,
       credential: import.meta.env.VITE_TURN_CREDENTIAL,
